4344e39ee9469ff5ab7016bd585f03091e7361e0
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 12:48:42 -0700

    Removed nested for loop

--- wordcount.py
+++ wordcount.py
@@ -1,12 +1,27 @@
 # put your code here.
-file_data = open('test.txt')
 
-word_count = {}
+def counting_words(file_data):
+    """Finds total count of each word in file.
 
-for line in file_data:
-    line = line.rstrip()
-    line = line.split(' ')
+    Iterates over file to determine the word count for each
+    word in the file using a dictionary. 
+    """
 
-    for word in line:
+    file_data = open(file_data)
+
+    word_count = {}
+
+    my_list = []
+
+    for line in file_data:
+        line = line.rstrip()
+        line = line.split(' ')
+        my_list.extend(line)
+
+    
+    for word in my_list:
         word_count[word] = word_count.get(word, 0) + 1
-        print word, word_count[word]
+        # print word, word_count[word]
+    return word_count
+
+print counting_words('test.txt')
